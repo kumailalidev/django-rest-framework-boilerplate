f7ee1fd0f09135cd37ca8e28c44fa123e1fe642c
kumailalidev <kumailalidev@gmail.com>
2024-05-04 18:05:22 +0500

    Added comments and docstrings for ping api endpoint view.

--- project/core/views.py
+++ project/core/views.py
@@ -1,9 +1,24 @@
-from rest_framework import status
-from rest_framework.request import HttpRequest
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import HttpRequest
 
 
 @api_view(http_method_names=["GET"])
 def ping(request: HttpRequest):
-    return Response(data={"ping": "pong"}, status=status.HTTP_200_OK)
+    """
+    A view function for handling ping requests.
+
+    This function responds to HTTP GET requests by returning a JSON response
+    with a "pong" message, indicating that the server is up and running.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        Response: A JSON response with a "ping" key and "pong" value.
+    """
+    # Create a JSON response with a "ping" key and "pong" value
+    response_data = {"ping": "pong"}
+    # Return the response with HTTP status code 200 OK
+    return Response(data=response_data, status=status.HTTP_200_OK)
